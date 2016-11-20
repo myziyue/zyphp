c61d3797311e2541590ec783da5ac82318270167
Bi Zhiming <bizhiming@vipbcw.com>
2016-11-20 09:01:51 +0800

    修改开源许可协议

--- www/index.php
+++ www/index.php
@@ -13,16 +13,8 @@
  * 2、 加载类库
  * 3、 启动框架
  */
-
-defined("ROOTPATH") or define('ROOTPATH', __DIR__ );
-defined("ZYPATH") or define('ZYPATH', ROOTPATH . '/../ziyuephp/');
-defined("APPPATH") or define('APPPATH', ROOTPATH . '/../app/');
-
-defined("ZYDEBUG") or define('ZYDEBUG', true);
-
-include_once ZYPATH . '/Zy.php';
-
-include_once ZYPATH . '/common/functions.php';
-
-Zy::run();
-p(Zy::version());
+defined("WEBROOT") or define("WEBROOT", __DIR__);
+//include_once WEBROOT . '/../vendor/autoload.php';
+include_once WEBROOT . '/../vendor/myziyue/zyphp/Ziyue.php';
+Ziyue::run();
+Ziyue::p(Ziyue::powered());
