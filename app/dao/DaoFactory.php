1660614ddd654941c442cd258fa623cea7d7e2c5
evan2884 <evan2884@gmail.com>
2017-03-09 18:50:05 +0800

    数据层封装

---
+++ app/dao/DaoFactory.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Bi Zhiming <evan2884@gmail.com>
+ * @created 2017/3/9  下午5:57
+ * @since 1.0
+ */
+
+namespace app\dao;
+
+
+use ziyue\exception\UnknownClassException;
+
+class DaoFactory
+{
+    private static $nameSpace = "\\app\\dao";
+    private static $daoInstrance = [];
+
+    /**
+     * @param $daoModel
+     * @param $daoClass
+     */
+    public static function get($daoModel, $daoClass){
+        $daoFullClass = self::$nameSpace . "\\" . strtolower($daoModel) . "\\" . ucfirst($daoClass) . "Dao";
+        if(!isset(self::$daoInstrance[$daoFullClass])){
+            if(!class_exists($daoFullClass)){
+                throw new UnknownClassException("Unable to find '$daoFullClass' in file: ".__FILE__.". Namespace missing?");
+            }
+            self::$daoInstrance[$daoFullClass] = new $daoFullClass;
+        }
+        return self::$daoInstrance[$daoFullClass];
+    }
+}
